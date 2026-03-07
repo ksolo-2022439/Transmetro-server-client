17f4023d317624261aa021f9b16047a9eb8c7b6a
OlivMer765 <omerida-2021641@kinal.edu.gt>
2026-03-07 16:08:51 -0600

    feat: Agregar validaciones
    - Se agregaron validaciones en wallet-validators.js para evitar inyecciones de montos negativos o nulos.
    
    - Se agrego una ruta POST en wallet.routes.js en donde se implementara a futuro el método de recarga de la tarjeta desde auth-service.

--- server-client/middlewares/wallet-validators.js
+++ server-client/middlewares/wallet-validators.js
@@ -5,3 +5,21 @@ export const getBalanceValidator =[
     body('userId', 'El ID de usuario es obligatorio').notEmpty().isString(),
     checkValidators
 ];
+
+export const rechargeWalletValidator = [
+    body('userId', 'El ID de usuario es obligatorio').notEmpty().isString(),
+    body('monto')
+        .exists().withMessage('El monto a recargar es obligatorio.')
+        .isNumeric().withMessage('El monto debe ser un valor numérico.')
+        .custom((value) => {
+            // Prevenir inyecciones de montos nulos, cero o negativos
+            if (value === null || value === undefined) {
+                throw new Error('El monto no puede ser nulo.');
+            }
+            if (value <= 0) {
+                throw new Error('El monto de recarga debe ser estrictamente mayor a Q0.00.');
+            }
+            return true;
+        }),
+    checkValidators
+];
--- server-client/src/wallets/wallet.routes.js
+++ server-client/src/wallets/wallet.routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { getBalance } from './wallet.controller.js';
-import { getBalanceValidator } from '../../middlewares/wallet-validators.js';
+import { getBalanceValidator, rechargeWalletValidator } from '../../middlewares/wallet-validators.js';
 import { validateJWT } from '../../middlewares/auth-validators.js';
 
 const router = Router();
 
 //GET
 router.get('/balance', validateJWT, getBalanceValidator, getBalance);
+
+
+// POST
+// Endpoint INTERNO (consumido por .NET) para recargar
+// router.post('/recharge', validateJWT, rechargeWalletValidator, [controlador]);
 
 export default router;
